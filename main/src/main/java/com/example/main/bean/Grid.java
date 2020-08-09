e6174f356329bc7e6d3aff4a3df87329bf38bbb5
陈泽宇 <624205157@qq.com>
2020-08-09 20:02:05 +0800

    网格

---
+++ main/src/main/java/com/example/main/bean/Grid.java
@@ -0,0 +1,26 @@
+package com.example.main.bean;
+
+/**
+ * Created by czy on 2020/8/8 20:45.
+ * describe: 网格
+ */
+public class Grid {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
--- main/src/main/java/com/example/main/fragment/grid/GridFragment.java
+++ main/src/main/java/com/example/main/fragment/grid/GridFragment.java
@@ -1,11 +1,37 @@
 package com.example.main.fragment.grid;
 
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.example.main.R;
+import com.example.main.adapter.GridAdapter;
+import com.example.main.bean.Grid;
 import com.example.main.fragment.BaseFragment;
 
-public class GridFragment extends BaseFragment {
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class GridFragment extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener {
+
+
+    @BindView(R.id.list)
+    RecyclerView list;
+    @BindView(R.id.refresh)
+    SwipeRefreshLayout refresh;
+
+    private GridAdapter adapter;
+    private List<Grid> mData = new ArrayList<>();
+
     @Override
     protected int setContentView() {
         return R.layout.fragment_grid;
@@ -13,6 +39,50 @@ public class GridFragment extends BaseFragment {
 
     @Override
     protected void lazyLoad(Bundle savedInstanceState) {
+        adapter = new GridAdapter(mData);
+        adapter.setAnimationEnable(true);
 
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(@NonNull BaseQuickAdapter<?, ?> adapter, @NonNull View view, int position) {
+
+            }
+        });
+        list.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        list.setAdapter(adapter);
+
+        refresh.setOnRefreshListener(this);
+
+        getData();
+    }
+
+    @OnClick(R.id.search)
+    public void onViewClicked() {
+
+
+    }
+
+    private void getData() {
+        Grid grid = new Grid();
+        grid.setName("qweqweqweqweqe");
+        grid.setAddress("发沙发沙发沙发沙发");
+        mData.add(grid);
+        mData.add(grid);
+        mData.add(grid);
+        mData.add(grid);
+        mData.add(grid);
+        mData.add(grid);
+
+        adapter.setList(mData);
+
+        adapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    public void onRefresh() {
+        getData();
+        refresh.setRefreshing(false);
     }
 }
