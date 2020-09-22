fcbb34c2aa2c9fd811a4c9878a921caef5e2e541
陈泽宇 <624205157@qq.com>
2020-09-22 22:50:51 +0800

    1

--- TRTClogin/src/main/java/com/tencent/liteav/login/GenerateTestUserSig.java
+++ TRTClogin/src/main/java/com/tencent/liteav/login/GenerateTestUserSig.java
@@ -41,7 +41,8 @@ public class GenerateTestUserSig {
      * 进入腾讯云云通信[控制台](https://console.cloud.tencent.com/avc ) 创建应用，即可看到 SDKAppId，
      * 它是腾讯云用于区分客户的唯一标识。
      */
-    public static final int SDKAPPID = 1400404366;
+//    public static final int SDKAPPID = 1400404366;
+    public static final int SDKAPPID = 1400419786;
 
 
     /**
@@ -63,7 +64,8 @@ public class GenerateTestUserSig {
      * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
      * 文档：https://cloud.tencent.com/document/product/269/32688#Server
      */
-    private static final String SECRETKEY = "755e819e05042c658c8fde340f705ed8a87ba412ff538d43bc4966d91bec8fc0";
+//    private static final String SECRETKEY = "755e819e05042c658c8fde340f705ed8a87ba412ff538d43bc4966d91bec8fc0";
+    private static final String SECRETKEY = "d49caabd070e8ba942fec6c7bd99e63d2a1b58e903f8b951854b1adc2e957634";
 
     /**
      * 计算 UserSig 签名
